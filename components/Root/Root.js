6a4e64a25752961d25fe921dfcb39b46df0fba5c
Tycho Bokdam <tycho@palmtreecoding.com>
2019-06-17 08:07:33 +0200

    feat: Added more meta data

--- components/Root/Root.js
+++ components/Root/Root.js
@@ -11,30 +11,23 @@ export const Root = ({ children }) => (
     <ResetJSS>
 
       <Head>
-
         <link
           href={'https://fonts.googleapis.com/css?family=Quicksand:300,400,700&subset=latin-ext'}
           rel={'stylesheet'}
           type={'text/css'} />
 
-        <link
-          rel={'shortcut icon'}
-          href={'/static/favicon.ico'} />
+        <link rel={'shortcut icon'} href={'/static/favicon.ico'} />
+        <meta name={'viewport'} content={'width=device-width, initial-scale=1.0'} />
 
         <title>Palm Tree Coding</title>
+        <meta name={'theme-color'} content={'#ffffff'} />
+        <meta name={'description'} content={'When standard software does not meet your requirements, we can assist you in consulting, building, hosting and managing custom solutions. So you can just sit back and relax (or focus on what makes your business grow).'} />
 
-        <meta
-          name={'viewport'}
-          content={'width=device-width, initial-scale=1.0'} />
-
-        <meta
-          name={'description'}
-          content={'When standard software does not meet your requirements, we can assist you in consulting, building, hosting and managing custom solutions. So you can just sit back and relax (or focus on what makes your business grow).'} />
-
-        <meta
-          name={'theme-color'}
-          content={'#ffffff'} />
-
+        <meta property={'og:site_name'} content={'Palm Tree Coding'} />
+        <meta property={'og:title'} content={'Palm Tree Coding'} />
+        <meta property={'og:description'} content={'When standard software does not meet your requirements, we can assist you in consulting, building, hosting and managing custom solutions. So you can just sit back and relax (or focus on what makes your business grow).'} />
+        <meta property={'og:type'} content={'website'} />
+        <meta property={'og:image'} content={'/static/images/banner.png'} />
       </Head>
 
       {children}
